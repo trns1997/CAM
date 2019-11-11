8b9196f2720782a16abd9cb9b2907c4b618667f0
trns1997 <trns@ust.hk>
2019-11-11 11:43:39 +0100

    small_update

--- CAM_inspection.m
+++ CAM_inspection.m
@@ -5,7 +5,7 @@ points_pl = planes{1};
 fun=@(params)d3Dp(points_pl,params);
 a=lsqnonlin(fun,[0 0 1 0]);
 
-z = @(x,y)((-a(1)*x - a(2)*y - a(4))/a(3)); % function handle to anonymous function
+z = @(x,y)((-a(1)*x - a(2)*y - a(4))/a(3));
 
 plot3(points_pl(:, 1), points_pl(:, 2), points_pl(:, 3), 'o')
 hold on
@@ -13,7 +13,6 @@ fsurf(z)
 axis square
 
 %% Transformation Matrix
-
 num_cyl = 10;
 points_cyl = cylinders{1};
 plot3(points_cyl(:, 1), points_cyl(:, 2), points_cyl(:, 3), 'o')
@@ -51,7 +50,6 @@ for j = 0:num_cyl-1
 
     fun=@(params)(d2Dpp(Coor_2D,params(1:2))-params(3));
     opt_par=lsqnonlin(fun,[0 0 10]);
-    roundness=range(d2Dpp(Coor_2D,opt_par(1:2)));
     % figure; axis equal;hold on
     % plot(Coor_2D(:,1),Coor_2D(:,2),'.');
     pc=[linspace(0,2*pi,100)' repmat(opt_par(3),100,1)];
@@ -65,16 +63,19 @@ for j = 0:num_cyl-1
 
     Coor_3D = T*[x,y,0,1]';
     p(j+1,:) = Coor_3D(1:3)' + dist*N;
-    a = Coor_3D(1:3)' + dist*N;
-
-    plot3(a(1), a(2), a(3), 'o')
 end
 
-%% Determine 
+plot3(p(:,1),p(:,2), p(:,3), 'o')
+
+%% Determine Deviation
 
 fun=@(params)(d3Dsl(p,N,params(1:3)));
 x0=fminimax(fun,[0 0 0]);
-p0 = x0+ N*80;
+dev=2*max(fun(x0));
+p0 = x0 + N*80;
 p1 = [x0; p0];
 plot3(p1(:,1),p1(:,2),p1(:,3), 'LineWidth',2)
+
+caption = sprintf('Deviation = %d', dev);
+title(caption, 'FontSize', 20);
 
